6b4d6db2105fc7106702ac1b4efedeb892449cdf
bcroal00 <bcroal00@email.cpcc.edu>
2023-09-11 20:23:31 -0400

    Added code to display all policy information the number of smokers, and the number of non-smokers

--- Project_Brent_Croal.java
+++ Project_Brent_Croal.java
@@ -38,7 +38,31 @@ public class Project_Brent_Croal
          }
       
          inputFile.close();
+               
+         int totalSmokers = 0;
+         int totalNonSmokers = 0;
+         for (Policy policy : Policies) {
+            if (policy.PolicyHolderSmokingStatus.equalsIgnoreCase("non-smoker"))
+               totalNonSmokers += 1;
+            else
+               totalSmokers += 1;
+               
          
+            System.out.println("Policy Number: " + policy.PolicyNumber);
+            System.out.println("Provider Name: " + policy.ProviderName);
+            System.out.println("Policyholder's First Name: " + policy.PolicyHolderFirstName);
+            System.out.println("Policyholder's Last Name: " + policy.PolicyHolderLastName);
+            System.out.println("Policyholder's Age: " + policy.PolicyHolderAge);
+            System.out.println("Policyholder's Smoking Status: " + policy.PolicyHolderSmokingStatus);
+            System.out.println("Policyholder's Height: " + policy.PolicyHolderHeight + " inches");
+            System.out.println("Policyholder's Weight: " + policy.PolicyHolderWeight + " pounds");
+            System.out.println("Policyholder's BMI: " + String.format("%.2f", policy.CalculateBMI()));
+            System.out.println("Policy Price: " + String.format("$%,.2f", policy.CalculatePolicyPrice()));
+            System.out.println();
+         }
+         
+          System.out.println("The number of policies with a smoker is: " + totalSmokers);
+          System.out.println("The number of policies with a non-smoker is: " + totalNonSmokers);
       }
       catch(IOException ex)
       {
